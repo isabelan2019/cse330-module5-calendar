62621da0adfc71941da2e7fdea071cc0eee782c1
Claire <claire.irawan@wustl.edu>
2020-10-29 19:51:50 -0500

    editevent basics (doesnt work)

--- login.php
+++ login.php
@@ -8,6 +8,7 @@ $json_str = file_get_contents('php://input');
 $json_obj = json_decode($json_str, true);
 $username = (string) $json_obj['username'];
 
+
 //prepared statement
 $stmt = $mysqli->prepare("SELECT COUNT(*), id, hashed_password from users where username=?");
 
@@ -28,13 +29,13 @@ $stmt->bind_result($cnt, $user_id, $pwd_hash);
 $stmt->fetch();
 
 //compare form password with database password
-$pwd_guess = $json_obj['password'];
+$pwd_guess = (string)$json_obj['password'];
 
 if($cnt==1 && password_verify($pwd_guess, $pwd_hash)){
     ini_set("session.cookie_httponly", 1);
     session_start();
     //login success
-    $_SESSION['user_id'] = $user_id;
+    $_SESSION['user_id'] = (int) $user_id;
     //generate token
     $_SESSION['token'] = bin2hex(random_bytes(32));
     echo json_encode(array(
